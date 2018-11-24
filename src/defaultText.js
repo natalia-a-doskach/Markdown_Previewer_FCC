6d0d831987422b7f36652bac968ecd4ce67c6b82
Natalia Doskach <natalia.a.doskach@gmail.com>
2018-11-24 16:22:54 +0800

    React added

---
+++ src/defaultText.js
@@ -0,0 +1,42 @@
+export const defaultText =
+`# Markdown guide
+
+## format your text
+
+**bold**
+
+_italic_
+
+~~crossed out~~
+
+## format your code
+
+Inline code, eg. \`<p>Hello World</p>\`.
+
+Or multi-line code:
+\`\`\`
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+## Lists and Block Quotes
+### block quotes:
+> block quote
+>> also block quote
+### Bulleted list:
+- one
+  - two
+     - three
+        - four
+
+### Numbered list:
+1. un
+1. deux
+3. trois
+4. quatre
+## Links and Pictures
+This is a link to my [GitHub](https://github.com/natalia-a-doskach).
+
+![Blue Picture](https://image.ibb.co/mbVr5V/Rectangle-4.jpg)`;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-/*ReactDOM.render(
-  <App / > ,
-          document.getElementById('root')
-        );*/
+import {defaultText} from './defaultText.js';
+import marked from 'marked';
+
+class Editor extends React.Component{
+render(){
+return (
+  <div id="editorDiv" class="window">
+    <div class="head">
+      <div class="circlesDiv">
+      <div class="circle red"></div>
+      <div class="circle orange"></div>
+      <div class="circle green"></div>
+    </div>
+      <h2>Editor</h2>
+    </div>
+    <div class="bodyDiv" id="textareaDiv">
+    <textarea class="text" defaultValue={this.props.text} onChange={this.props.onChange}/>
+    </div>
+  </div>
+)
+}
+}
+
+
+class Previewer extends React.Component{
+render(){
+return (
+  <div id="previewerDiv" class="window">
+    <div class="head">
+            <div class="circlesDiv">
+      <div class="circle red"></div>
+      <div class="circle orange"></div>
+      <div class="circle green"></div>
+    </div>
+      <h2>Previewer</h2>
+    </div>
+    <div class="bodyDiv" id="textDiv" >
+      <div dangerouslySetInnerHTML={{__html:marked(this.props.text)}} id="previewer" class="text"></div>
+    </div>
+  </div>
+)
+}
+}
+
+class App extends React.Component{
+constructor(props){
+super(props);
+this.state = {text: defaultText}
+this.handleChange = this.handleChange.bind(this)}
+
+handleChange(e){
+this.setState({text: e.target.value});
+}
+render() { return(
+<div id="appDiv">
+<Editor text={this.state.text} onChange={this.handleChange}/>
+<Previewer text={this.state.text}/>
+</div>)
+}
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"))
